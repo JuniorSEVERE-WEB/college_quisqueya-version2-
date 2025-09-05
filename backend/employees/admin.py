9659dc67472a886c1c197f78ceabef5bf3ea3d63
Junior SEVERE <severejunior2017@gmail.com>
2025-09-05 19:45:15 -0400

    add a #

--- backend/employees/admin.py
+++ backend/employees/admin.py
@@ -6,7 +6,7 @@ class EmployeeAdmin(admin.ModelAdmin):
     list_display = ("user", "position", "academic_year", "hire_date", "created_at")
     list_filter = ("academic_year", "position")
     search_fields = ("user__username", "position")
-
+##
     # Pour limiter les choix de user dans l’admin → uniquement role=employee
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         from accounts.models import User
