0a7100e4db02df280b51140c014f059650837038
Votre Nom <votre.email@example.com>
2025-10-18 21:21:33 -0400

    fix render deployment render.yaml, wsgi, build.sh

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,13 +1,12 @@
 """
 WSGI config for backend project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
 import os
 from django.core.wsgi import get_wsgi_application
 
-# ✅ Utilise les paramètres de déploiement pour Render
+# ✅ Utiliser le bon fichier de settings pour Render
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.deployment_settings")
 
 application = get_wsgi_application()
