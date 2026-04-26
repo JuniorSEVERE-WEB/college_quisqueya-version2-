90b90791b4d42750f83ad3ed57594923a4832176
Junior SEVERE <severejunior2017@gmail.com>
2026-04-26 12:31:06 -0400

    Serve media files explicitly in production

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 # backend/backend/urls.py
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.i18n import set_language
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -11,7 +11,7 @@ from rest_framework_simplejwt.views import (
 from django.contrib.auth import views as auth_views
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.views.generic import TemplateView
 from django.http import JsonResponse
 
@@ -119,4 +119,6 @@ urlpatterns = [
 
 # ---- Fichiers médias ----
 if settings.SERVE_MEDIA_FILES:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
